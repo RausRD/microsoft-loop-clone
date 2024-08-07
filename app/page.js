efc564eb6fcf73d27f92f4a04aba9cf7133b2af7
RausRD <dunecruslan2@gmail.com>
2024-08-07 17:07:03 +0300

    add registration

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home () {
 	return (
-	<h1>Hello</h1>
+		<div>
+			<h1>Hello</h1>
+			<Button variant='outline'>Call</Button>
+		</div>
 	);
 }
